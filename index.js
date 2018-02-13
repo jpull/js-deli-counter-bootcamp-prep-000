c2a1eb9a0954e5422f895b2a9d77c4698a50349b
Jerry P <jpull@jpull.com>
2018-02-13 23:41:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ function nowServing(katzDeliLine) {
   }
 }
 function currentLine(katzDeliLine) {
+  line = "The line is currently:"
   if (katzDeliLine.length == 0) { return `The line is currently empty.`; }
   else {
     for (var i = 0; i < katzDeliLine.length; i++) {
-    return katzDeliLine[i];
+    line = " " + line + i + "." + line[i] + ",";
   }
+  return line;
 }
 }
