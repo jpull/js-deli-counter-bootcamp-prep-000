06e62a098505d78d9070c6577f5cd7961730a040
Jerry P <jpull@jpull.com>
2018-02-13 23:41:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(katzDeliLine) {
   }
 }
 function currentLine(katzDeliLine) {
-  line = "The line is currently:"
+  var line = "The line is currently:"
   if (katzDeliLine.length == 0) { return `The line is currently empty.`; }
   else {
     for (var i = 0; i < katzDeliLine.length; i++) {
